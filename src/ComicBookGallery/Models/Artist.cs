99b5f37e82d01b1e41b9d93be79eadf7f0c8ad9e
martintaylor <martintaylor1635@gmail.com>
2018-12-13 22:04:30 +0000

    Added artist and comic book models

---
+++ src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+
+        // PROPERTIES
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+    }
+}
